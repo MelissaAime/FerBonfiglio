08cb85ef0c68cbe4f4ff4778faff3c9bf70d19e3
Melissa Aime <aimemelissa1@gmail.com>
2023-02-07 18:15:41 -0300

    home section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 export const Home = () => {
     return(
-        <>
-        </>
+        <div className="home">
+            <h1 className="home-title">Fer Bonfiglio</h1>
+        </div>
     )
 }
