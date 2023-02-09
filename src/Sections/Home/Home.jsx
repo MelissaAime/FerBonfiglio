8b009387e4519575c7e5f172e80da7e1eb0ea97d
Melissa Aime <aimemelissa1@gmail.com>
2023-02-09 14:06:09 -0300

    soundwave home

--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -1,7 +1,10 @@
+import { SoundWave } from "../../Components/SoundWave/soundwave"
+
 export const Home = () => {
     return(
         <div className="home">
             <h1 className="home-title">Fer Bonfiglio</h1>
+            <SoundWave/>
         </div>
     )
 }
