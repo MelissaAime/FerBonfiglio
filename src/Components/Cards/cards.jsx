426e1ad4c66af6cceac13a3cbd1114866858e16d
Melissa Aime <aimemelissa1@gmail.com>
2023-02-12 11:52:16 -0300

    projects cards

---
+++ src/Components/Cards/cards.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export const Cards = ({title, text, image, alt}) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} alt={title}/>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{text}</Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -1,7 +1,28 @@
+import { Cards } from "../../Components/Cards/cards";
+
 export const Projects = () => {
+
+    const projects = [
+        {
+            'title': 'uno',
+            'text' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ex asperiores fuga perspiciatis illum fugiat quas modi, quo possimus dolor excepturi odit numquam laborum? Voluptas minima labore tempora aut non?',
+            'image' : 'holder.js/100x180',
+        },
+        {
+            'title': 'dos',
+            'text' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ex asperiores fuga perspiciatis illum fugiat quas modi, quo possimus dolor excepturi odit numquam laborum? Voluptas minima labore tempora aut non?',
+            'image' : "holder.js/100x180",
+        }
+    ]
+
     return(
         <div className="projects">
             <h3 className="projects-title">My projects</h3>
+
+            {projects.map(({title, text, image, alt}) => (
+                <Cards title={title} text={text} image={image} alt={title}/>
+            ))}
+            
         </div>
     )
 }
