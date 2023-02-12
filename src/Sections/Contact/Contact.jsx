d3e92f8a97492f4aea0ea68de42639e04d83cff8
Melissa Aime <aimemelissa1@gmail.com>
2023-02-12 13:52:50 -0300

    contact box

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,7 +1,10 @@
 export const Contact = () => {
     return (
         <div className="contact" id="contact">
-
+            <div className="contact-box">
+                <div className="contact-box-info"></div>
+                <div className="contact-box-form"></div>
+            </div>
         </div>
     )
 }
